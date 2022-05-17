2d7ddf8ba7c3a7a58549487e28c4335e1cbca870
Elpis-alpha <elpis409@gmail.com>
2022-05-17 19:17:12 +0100

    end

--- source/components/general/HeadTag.js
+++ source/components/general/HeadTag.js
@@ -15,9 +15,17 @@ const HeadTag = ({ title, description }) => {
 
       <meta name="author" content={creator} />
 
-      <meta name='robots' content='index,follow' />
+      <meta name="publisher" content={creator} />
+
+      <meta name='robots' content='all' />
+
+      <meta name='googlebot' content='all' />
 
       <meta name="theme-color" content="#dadcdd" />
+
+      <meta name='application-name' content={siteName} />
+
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
@@ -30,6 +38,24 @@ const HeadTag = ({ title, description }) => {
       <link rel="apple-touch-icon" href="/logo192.png" />
 
       <link rel="manifest" href="/manifest.json" />
+
+      <meta property="og:title" content={siteName} />
+
+      <meta property="og:url" content="https://elpis-portfolio.vercel.app/" />
+
+      <meta property="og:description" content={description ? description : siteDescription} />
+
+      <meta property="og:type" content="website" />
+
+      <meta property="og:locale" content="en_US" />
+
+      <meta property="og:image" content="https://elpis-portfolio.vercel.app/images/open-graph.png" />
+
+      <meta property="og:image:width" content="1200" />
+
+      <meta property="og:image:height" content="630" />
+
+      <meta property="og:image:type" content="image/png" />
 
     </Head>
 
